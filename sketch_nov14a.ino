4ad983faba869498d21cbbdfa34353fddfbad8fb
PRINCE GOYAL <141664327+Prince-74@users.noreply.github.com>
2024-12-19 22:05:36 +0530

    Update sketch_nov14a.ino

--- sketch_nov14a.ino
+++ sketch_nov14a.ino
@@ -3,8 +3,8 @@
 #include <HTTPClient.h>
 #include <HardwareSerial.h>
 
-const char* ssid = "Redmi 12 5G";           // Replace with your Wi-Fi SSID
-const char* password = "74171936";   // Replace with your Wi-Fi Password
+const char* ssid = "xxxxx";           // Replace with your Wi-Fi SSID
+const char* password = "xxxx";   // Replace with your Wi-Fi Password
 
 TinyGPSPlus gps;
 HardwareSerial gpsSerial(1);
